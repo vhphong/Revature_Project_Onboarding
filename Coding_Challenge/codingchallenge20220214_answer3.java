ccfc9a352243b2acd21ca3fffa4174da71ec9e45
vhphong <vhphong@gmail.com>
2022-02-15 10:54:38 -0500

    added: codingchallenge20220214_answer3.java

--- Coding_Challenge/codingchallenge20220214_answer3.java
+++ Coding_Challenge/codingchallenge20220214_answer3.java
@@ -1,11 +1,11 @@
 public class codingchallenge20220214_answer3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // System.out.println("Hello");
 
-        actualMemorySize("1GB");
-        // actualMemorySize("2GB");
-        // actualMemorySize("512MB");
+        actualMemorySize("32GB");
+        actualMemorySize("2GB");
+        actualMemorySize("512MB");
     }
 
     public static void actualMemorySize(String memSizeParam) {
@@ -13,14 +13,14 @@ public class codingchallenge20220214_answer3 {
             System.out.println("Invalid memory size input");
         } else {
             String unitString = memSizeParam.substring(memSizeParam.length() - 2);
-            System.out.println(unitString);
+            // System.out.println(unitString);
 
             String memSizeStr = memSizeParam.substring(0, memSizeParam.length() - 2);
-            System.out.println(memSizeStr);
+            // System.out.println(memSizeStr);
 
             // convert memSize (as string) to number
             Double memSize = Double.parseDouble(memSizeStr);
-            System.out.println("memSize " + memSize);
+            // System.out.println("memSize " + memSize);
 
             Double actualMemSize;
 
@@ -31,8 +31,14 @@ public class codingchallenge20220214_answer3 {
 
             if ((memSize * .93 < 1) && (unitString.equalsIgnoreCase("GB"))) {
                 unitString = "MB";
-                actualMemSize = memSize * 1000 * .93;
+                actualMemSize = (double) Math.round(memSize * 1000 * .93);
                 System.out.println(actualMemSize + unitString);
+            }
+
+            if (unitString.equalsIgnoreCase("MB")) {
+                Double temp = memSize * .93;
+                int result = temp.intValue();
+                System.out.println(result + unitString);
             }
         }
     }
