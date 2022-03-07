5a59eee9da1ab540b3086834473cbc0eba3d4690
PhongHVo <vhphong@gmail.com>
2022-03-07 18:38:36 -0500

    added: codingchallenge20220307_FibonacciSum

---
+++ Coding_Challenge/codingchallenge20220307_FibonacciSum.java
@@ -0,0 +1,35 @@
+public class codingchallenge20220307_FibonacciSum {
+
+    public static void main(String[] args) {
+
+        // call EvenFibonacciSum
+        EvenFibonacciSum();
+
+    }
+
+
+    public static void EvenFibonacciSum() {
+
+        long init1 = 1L;
+        long init2 = 2L;
+        long limit = 4000000000L;
+        long runningFibTerm = 0L;
+        long sum = 0L;
+
+        while (runningFibTerm < limit) {
+            runningFibTerm = init1 + init2;
+
+            if (runningFibTerm % 2 == 0) {
+                sum +=runningFibTerm;
+            }
+
+
+            init1 = init2;
+            init2 = runningFibTerm;
+        }
+
+        System.out.println("EvenFibonacciSum is " + sum);
+
+    }
+
+}
